02ce26ec64023378f10e307ac7c5b2ee98f28bba
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-05-31 20:40:14 +0800

    feat: updated the cors

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000",
+		AllowOrigins: "https://todo-list-react-go-client.vercel.app/",
 		AllowHeaders: "Origin,Content-Type,Accept",
 	}))
 
